e614f58762bcb2b995de37c983e6dc3e20fc5ee1
Tornike1123 <toko36106@gmail.com>
2022-01-19 23:57:04 +0400

    change

--- mine.js
+++ mine.js
@@ -14,7 +14,7 @@ function validate(){
     let chr = document.getElementById('char');
     let ent = document.getElementById('enter');
     let eye = document.getElementById('eyes');
-    let psw =   /^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*,./<>:}{"';აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ])[a-zA-Z0-9!@#$%^&*,./<>:}{"';აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ]{8,25}$/;
+    let psw =   /^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*,./<>:+-_=}{"';აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ])[a-zA-Z0-9!@#$%^&*,./<>:}{"';+-_=აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ]{8,25}$/;
     
 
     if (passw.value.match(psw)){
